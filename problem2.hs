ad6468318c23db198ae1d9af0b7d806cf7b42fc8
AbstractLogix <oscar7557@gmail.com>
2016-02-14 13:20:54 -0500

    messy answer, will refactor someday.

--- problem2.hs
+++ problem2.hs
@@ -1,5 +1,4 @@
 -- definitely looking for a more efficient way to write this
-answerProb2 = foldr1 (+) [x | x <- prob2, x `rem` 2 == 0, x < 4000000]
-
+answerProb2 = sum $ filter (even) $filter (<3999999) $ take 35 prob2
 -- canonical zipWith implementation
 prob2 = 0 : 1 : zipWith (+) prob2 (tail prob2)
