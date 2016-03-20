086ce65a2b4fd6809961764d3b11b55b4523e1bb
AbstractLogix <oscar7557@gmail.com>
2016-03-20 17:04:08 -0400

    Problem10 sum of primes

---
+++ Problem10.hs
@@ -0,0 +1,18 @@
+-- Compile for best results -o
+
+main  = do
+  print $ primeGenerator
+
+-- The functions primes and isprime are taken from this implementation of
+-- the Sieve of Eratosthenes https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+
+primes :: [Integer]
+primes    = 2 : [x | x <- [3..], isprime x]
+
+isprime :: Integer -> Bool
+isprime x = all (\p -> x `mod` p > 0) (factorsToTry x)
+  where
+    factorsToTry x = takeWhile (\p -> p * p <= x) primes
+
+
+primeGenerator = sum $ takeWhile (<1999999) primes
